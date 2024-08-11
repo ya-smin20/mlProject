55bfb719de9528572e200ec6583c42b14f806839
Yasmine <boujartyasmine@gmail.com>
2024-08-11 21:01:20 +0200

    model trainer

--- src/components/data_ing.py
+++ src/components/data_ing.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transf import DataTransfomationConfig, DataTransformation
+from src.components.model_trainer import ModelTrainerConfig,ModleTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -44,4 +45,6 @@ if __name__== "__main__":
     train_data, test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_trans(train_path=train_data,test_path=test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_trans(train_path=train_data,test_path=test_data)
+    modeltrainer=ModleTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/exception.py
+++ src/exception.py
@@ -1,21 +1,18 @@
 import sys
 from src.logger import logging
 
-def error_msg(error,err_detail:sys):
-    _,_,exc_tb=err_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    err_message = "Error in python script [{0}] line number [{1}] error msg [{2}]".format(
-    file_name, exc_tb.tb_lineno, str(error)
-)
-
-
+def error_msg_detail(error, err_detail: sys):
+    _, _, exc_tb = err_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    err_message = "You have an Error in python script [{0}] line number [{1}] error msg [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
     return err_message
 
-class CustomException (Exception):
-    def __init__(self, error_msg, err_detail:sys):
-        super().__init__(error_msg)
-        self.error_msg=error_msg(error_msg,err_detail=err_detail)
+class CustomException(Exception):
+    def __init__(self, error, err_detail: sys):
+        super().__init__(error)
+        self.error_msg = error_msg_detail(error, err_detail=err_detail)
+
     def __str__(self):
         return self.error_msg
-    
-
